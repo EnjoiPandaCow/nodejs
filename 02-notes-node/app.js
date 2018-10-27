4092f68229a2281c7367cdbe52317c04b74163ab
CJ O'Sullivan <me@cjosullivan.net>
2018-10-27 15:46:06 +0100

    Completed lecture-12. Installed nodemon.

--- 02-notes-node/app.js
+++ 02-notes-node/app.js
@@ -1,17 +1,7 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
-const os = require('os');
-const notes = require('./notes.js');
 const _ = require('lodash');
 
-console.log(_.isString(true));
-console.log(_.isString('CJ'));
+const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['CJ', 1, 'CJ', 1, 2, 3, 4]);
-console.log(filteredArray);
 
-console.log('Result:', notes.add(3, 8));
 
-// let user = os.userInfo();
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`);
