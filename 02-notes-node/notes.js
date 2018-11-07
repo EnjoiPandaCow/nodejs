272232652d913fb3c71b8d11e31c10e9e660887d
CJ O'Sullivan <me@cjosullivan.net>
2018-11-07 18:21:14 +0000

    Completed lecture 16

--- 02-notes-node/notes.js
+++ 02-notes-node/notes.js
@@ -1,5 +1,32 @@
+const fs = require('fs');
+
 const addNote = (title, body) => {
-    console.log('Adding note', title, body);
+
+    let notes = [];
+    const note = {
+      title,
+      body
+    };
+
+    try {
+        const notesString = fs.readFileSync('notes-data.json');
+        notes = JSON.parse(notesString);
+    } catch (e) {
+
+    }
+
+    /* Same as below using ES6 arrow functions.
+    let duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    });
+    */
+
+    let duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 const getAll = () => {
